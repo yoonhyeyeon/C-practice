a90411be703182803515544fb7cb9e9b537901ac
yoonhyeyeon <163373010+yoonhyeyeon@users.noreply.github.com>
2025-05-19 17:03:33 +0900

    250519_2

---
+++ Archiving data/StringFormatBasic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormatBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0,-20}{1,-15}{2,30}";
+
+            WriteLine(fmt, "Publisher", "Author","Title");
+            WriteLine(fmt, "Marvel", "Stan Lee","Iron Man");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+            WriteLine(fmt, "It", "Hyeyeon", "C# Practice");
+
+        }
+    }
+}
---
+++ Archiving data/StringSlice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSlice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning.";
+
+            WriteLine(greeting.Substring(0, 5));    // "Good"
+            WriteLine(greeting.Substring(5));       // "morning"
+            WriteLine();
+
+            string[] arr = greeting.Split(
+                new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+
+        }
+    }
+}
